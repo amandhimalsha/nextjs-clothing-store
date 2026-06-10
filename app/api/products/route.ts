eb185168b9978ad535aa2492de3b51f9c02a5acf
amandhimalsha <amandhimalsha.t@gmail.com>
2026-06-10 15:42:31 +0530

    POST request -- phase 5 completed

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -3,3 +3,14 @@ import { products } from "@/data/products";
 export async function GET() {
   return Response.json(products);
 }
+
+export async function POST(
+  request: Request
+) {
+  const body = await request.json();
+
+  return Response.json({
+    message: "Product created",
+    product: body,
+  });
+}
